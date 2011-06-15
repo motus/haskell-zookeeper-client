140884e32336a631703b34a689cee38b6da2da82
Sergei Matusevich <sergei.matusevich@gmail.com>
2011-06-14 23:54:35 -0700

    minor fixes to satisfy flymake (underscore unused variables, declare tupe for main :: IO () etc

--- ZooClient.hs
+++ ZooClient.hs
@@ -3,12 +3,13 @@ module Main where
 
 import qualified Zookeeper as Zoo
 
+main :: IO ()
+
 main = do
   zh   <- Zoo.init "localhost:3181" nullWatcher 10000
   path <- Zoo.create zh "/xyz" (Just "value")
           Zoo.OpenAclUnsafe Zoo.defaultCreateMode
-  (val, stat) <- Zoo.get zh "/xyz" Zoo.NoWatch
-  print (val, stat)
+  (val, stat) <- Zoo.get zh path Zoo.NoWatch
+  print (path, val, stat)
   -- Zoo.close zh
-  where nullWatcher zh zEventType zState path = putStrLn path
-
+  where nullWatcher _zh _zEventType _zState path = putStrLn path
